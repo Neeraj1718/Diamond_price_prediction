88018f1c57e0fbd90a960cb73a8a84110077ca30
Neeraj Kumar Pal <neerajpal1801@gmail.com>
2023-04-21 11:47:03 +0530

    app.py and web application

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -60,6 +60,8 @@ class DataIngestion:
 
 if __name__ == '__main__':
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_tranformation = DataTransformation()
+    train_arr,test_arr,_=data_tranformation.initaite_data_transformation( train_data_path,test_data_path)
 
 
